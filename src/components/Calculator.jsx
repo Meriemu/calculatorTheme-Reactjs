6cdc470b797d24f6991ae428e539fe64a309b960
meruem92 <me.achemlal.com>
2022-12-23 17:09:38 +0100

    Calculator : set state

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,52 +3,41 @@ import ThemeSwitcher from './ThemeSwitcher';
 import CalculatorResult from './CalculatorResult';
 
 const Calculator = () => {
-   const [nb, setNb ]= useState(0);
-   const handleOperation=(nbr)=> {
-      setNb(nbr);
-      // setNb(prevState =>  ({nbr: prevState.nbr +1 }) );
-      console.log("couleur : ", nbr)
+   const btnValues = [
+      ["C", "%", "/"],
+      [7, 8, 9, "x"],
+      [4, 5, 6, "-"],
+      [1, 2, 3, "+"],
+      [0, ".", "="],
+    ];
 
-      if(nbr === 'reset') {
-         setNb(0);
-      }
-   }
-//   useEffect(() => {
-//    handleOperation(nb);
-//   }, [nb]);
   return (
     <div className='calculator__wrapper'>
       <ThemeSwitcher />
-      <CalculatorResult showNb={nb} />
+      <CalculatorResult  />
 
       <div className='calculatorNB__container'>
          <div className='calculatorNB__row'> 
-            <button onClick={()=>handleOperation(7)} value='7'> 7 </button>
-            <button onClick={()=>handleOperation(8)} value='8'> 8 </button>
-            <button onClick={()=>handleOperation(9)} value='9'> 9 </button>
-            <button> DEL </button>
-         </div>
-         <div className='calculatorNB__row'> 
-            <button onClick={()=>handleOperation(4)} value='4'> 4 </button>
-            <button onClick={()=>handleOperation(5)} value='5'> 5 </button>
-            <button onClick={()=>handleOperation(6)} value='6'> 6 </button>
-            <button> + </button>
-         </div>
-         <div className='calculatorNB__row'> 
-            <button onClick={()=>handleOperation(1)} value='1'> 1 </button>
-            <button onClick={()=>handleOperation(2)} value='2'> 2 </button>
-            <button onClick={()=>handleOperation(3)} value='3'> 3 </button>
-            <button> - </button>
-         </div>
-         <div className='calculatorNB__row'> 
-            <button onClick={()=>handleOperation('.')}> . </button>
-            <button onClick={()=>handleOperation(0)}> 0 </button>
-            <button onClick={()=>handleOperation('/')}> / </button>
-            <button onClick={()=>handleOperation('x')}> x </button>
-         </div>
-         <div className='calculatorNB__row'> 
-            <button onClick={()=>handleOperation('reset')}> RESET </button>
-            <button onClick={()=>handleOperation('=')}> = </button>
+             {
+               btnValues.flat().map((btn,i)=> {
+                  return   <button key={i} 
+                     onClick={()=>{
+
+                        btn === "+" || btn === "-" || btn === "x" || btn === "/"
+                           ? signsHandler
+                        : btn === "%"
+                           ? moduloHandler
+                        :  btn === "." 
+                           ?  comaHandler
+                        :  btn === "="
+                           ?  equalsHandler
+                        :  btn === "C"
+                           ?  resetHandler
+                        :  numClickHandler
+
+                     }}> { btn }</button>
+               })
+            } 
          </div>
       </div>
     </div>
